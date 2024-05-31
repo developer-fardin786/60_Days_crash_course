06a8bde8b7228378294586ee38d6c4711df44cf8
Md Fardin Alam <developerfardin2.0@gmail.com>
2024-05-31 07:49:17 +0530

    react assignment2 task1

---
+++ React/Assignment2/task1/a2p1/src/components/User.jsx
@@ -0,0 +1,14 @@
+function User({ avatar, email, first_name, last_name }) {
+  console.log()
+  return (
+    <div>
+      <img src={avatar} alt={first_name} />
+      <div>
+        Name : {first_name} {last_name}
+      </div>
+      <div>Email : {email}</div>
+    </div>
+  );
+}
+
+export default User;
